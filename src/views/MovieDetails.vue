2f81f2c790a7d134e14b6a337f48bf081789e631
shubhanshuyadav <shubhanshuemailid@gmail.com>
2022-01-23 16:01:40 +0530

    initial movie details and list page added

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,3 +1,117 @@
 <template>
-    <div>this is movie detail page</div>
+  <div class="pa-2">
+    <v-row v-if="movie" class="mt-2">
+      <v-col md="5">
+        <v-card
+          hover
+          :loading="loading"
+          class="mx-auto"
+          width="100%"
+          height="auto"
+        >
+          <v-img height="100%" :src="movie.Poster" contain> </v-img>
+        </v-card>
+      </v-col>
+      <v-col md="7">
+        <v-row align="start" class="mt-1">
+          <v-card>
+            <v-card-text>
+              <v-card-title class="ma-0 pa-0">
+                {{ movie.Title }}
+              </v-card-title>
+              <v-row align="center pa-2">
+                <v-rating
+                  :value="movie.imdbRating"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="">
+                  {{ movie.imdbRating }} ({{ movie.imdbVotes }})
+                </div>
+              </v-row>
+            </v-card-text>
+            <div class="pl-4" style="text-align: left;">{{ movie.Plot }}</div>
+            <v-divider class="mt-4"></v-divider>
+            <v-card-text style="text-align: left;">
+              <v-row>
+                <v-col md="3">
+                  <div>Director:</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Director }}</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="3">
+                  <div>Writer:</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Writer }}</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="3">
+                  <div>Actors</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Actors }}</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="3">
+                  <div>Production:</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Production }}</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="3">
+                  <div>Released At:</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Released }}</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="3">
+                  <div>Country:</div>
+                </v-col>
+                <v-col md="9">
+                  <div>{{ movie.Country }}</div>
+                </v-col>
+              </v-row>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      movie: null
+    };
+  },
+  mounted(){
+      this.getMovieByImdbId()
+      console.log(this.$route.params)
+  },
+  methods: {
+      async getMovieByImdbId(){
+          this.axios.get("http://www.omdbapi.com/?apikey=4d3f75e4&i=" + this.$route.params.movie_id)
+          .then((response)=> {
+              this.movie = response.data;
+          })
+      }
+  }
+};
+</script>
--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,5 +1,86 @@
 <template>
-    <div>
-        this is movie list page
-    </div>
+  <div>
+    <v-container class="grey lighten-5 mb-6">
+      <v-row>
+        <v-col md="11">
+          <v-text-field
+            v-model="search"
+            label="Search Movie"
+            placeholder="Type movie title....."
+            rounded
+            append-outer-icon="mdi-send"
+          ></v-text-field>
+        </v-col>
+        <v-col md="1">
+          <v-btn elevation="2" @click="searchMovies()">Search</v-btn>
+        </v-col>
+      </v-row>
+      <v-row no-gutters v-if="movies">
+        <v-col md="3" class="pa-1" v-for="movie in movies.Search" :key="movie.imdbID">
+          <v-card :to="{name:'movie-details',params:{movie_id:movie.imdbID}}" hover :loading="loading" class="mx-auto my-12" max-width="374" max-height="300px">
+            <v-img
+              height="100%"
+
+              :src="movie.Poster"
+              contain
+            >
+            <v-container class="grey lighten-5 ma-0 pa-0">
+                <v-row
+                    align="end"
+                    no-gutters
+                    style="height: 300px;"
+                >
+                    <v-col>
+                    <v-card
+                        color="transparent"
+                        class="text-weight-bold"
+                        style="color:white"
+                        flat
+                        width="100%"
+
+                    >
+                        <div class="headline movie-title-bg">{{movie.Title}}</div>
+                    </v-card>
+                    </v-col>
+                </v-row>
+                </v-container>
+            </v-img>
+
+
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      search: "",
+      movies: null
+    };
+  },
+  computed: {
+    loggedInUser: () => {
+      return JSON.parse(localStorage.getItem("loggedInUser"));
+    },
+  },
+  methods: {
+    async searchMovies() {
+      let movies = await this.axios
+        .get("http://www.omdbapi.com/?apikey=4d3f75e4&s=" + this.search)
+        .then((response) => {
+          return response.data;
+        });
+      console.log(movies);
+      this.movies = movies;
+    },
+  },
+};
+</script>
+<style scoped>
+    .movie-title-bg {
+        background: rgb(0 0 0 / 79%);
+    }
+</style>
